a4736b8e3f4391f7e69c71d4e31fddc862c174cf
Pablo Haramboure <pharamboure@REDES.DPE>
2025-11-18 11:48:14 -0300

    Día 1 - Variables en Python

---
+++ 00_hello_world.py
@@ -0,0 +1,18 @@
+print('Hello World')
+
+# Consultar el tipo de dato de la cadena "Hello World"
+print(type("Hello World")) # Esto imprimirá: <class 'str'>
+
+print(type(42)) # Esto imprimirá: <class 'int'>
+
+print(type(3.14)) # Esto imprimirá: <class 'float'>
+
+print(type(True)) # Esto imprimirá: <class 'bool'>
+print(type(1 + 2j)) # Esto imprimirá: <class 'complex'>
+
+"""
+Esto es un programa simple en Python que imprime "Hello World" en la consola.
+En varias
+Lineas
+"""
+
---
+++ 01_variables.py
@@ -0,0 +1,12 @@
+# Variables in Python
+
+my_string_variable = "My String Variable"
+print(my_string_variable)  # This will print: My String Variable
+
+my_int_variable = 25
+print(my_int_variable)  # This will print: 25
+
+my_bool_variable = False
+print(my_bool_variable)  # This will print: False
+
+print(my_string_variable, my_int_variable, my_bool_variable)
